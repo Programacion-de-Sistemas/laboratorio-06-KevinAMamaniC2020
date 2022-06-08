aeaf0b54d5dc9d2184f79a2477eb4ffb1605edbd
KevinAMamaniC2020 <kmamanicondo@unsa.edu.pe>
2022-06-07 19:27:59 -0500

    Sobreponiendo imagenes con superImpose

--- ejemplo.c
+++ ejemplo.c
@@ -7,15 +7,24 @@ void display(){
   interpreter(blackKnight);*/
 
   //cuadro blanco y negro
-  char** Black = reverse(whiteSquare);
+  /*char** Black = reverse(whiteSquare);
   char** Union = join(Black, whiteSquare);
   
+  //Union de filas
   char** Suceso = repeatH(Union,4);
   char** Espejo =flipV(Suceso);
   
+  //Union de columnas
   char** UnionFilas = up(Suceso , Espejo);
   char** Tablero = repeatV(UnionFilas,4);
-  interpreter(Tablero); 
+  interpreter(Tablero); */
+
+  char** CuadroB=reverse(whiteSquare);
+
+  char** torreW=rook;
+
+  char** juntar=superImpose(rook,whiteSquare);
+  interpreter(juntar);
 
 }
   
