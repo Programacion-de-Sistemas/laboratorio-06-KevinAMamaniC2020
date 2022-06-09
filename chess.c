1ccb8181f37412e45297a3324868ff51e6d5ef4b
KevinAMamaniC2020 <kmamanicondo@unsa.edu.pe>
2022-06-08 20:31:23 -0500

    Se esta agregando las funciones rotate right y left

--- chess.c
+++ chess.c
@@ -2,28 +2,43 @@
 #include "figures.h"
 
 char** flipV(char** y){
+    // Se crea un puntero para recorrer la cadena dentro del array
     char *p;
     p = y;
+    //recorre todo el array
     for(int i=0;i<sizeof(y);i++){
+        //recorremos con el puntero la cadena
         while(*p != '\0'){
-            y[i] = strev(p);
-            p++;
+            y[i] = strev(p);    //se invierte la cadena en una posicion
+            p++;                
         }        
     }
-    return y;    
+    return y;       //retorna la cadena    
 }
 
 char** flipH(char** y){
+    //recorre todo el array e invierte sus elementos
     for(int i=sizeof(y); i>=0;i--)
         return y;
 }
 
 char** rotateL(char** y){
-        
+    char temp = y[0];
+    for(int i=0; i<(sizeof(y)-1);i++)
+        y[i] = y[i+1];
+    return y;
 }
 
 char** rotateR(char** y){
-
+    int tamaño = sizeof(y);
+    int n = tamaño;
+    char temp = y[tamaño-1];
+    for(int j=0; j<n;i++){
+        for (int i=(tamaño-2);i>=0;i--)
+            y[i+1] = y[i];
+        y[0]=temp;
+    }
+    return y;
 }
 
 char** reverse(char** y){
