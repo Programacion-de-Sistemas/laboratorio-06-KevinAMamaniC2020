ad710152e343c0d67b4821ea4d9923c567059a33
KevinAMamaniC2020 <kmamanicondo@unsa.edu.pe>
2022-06-08 20:11:21 -0500

    Se modificaron cosas del chess.c

--- chess.c
+++ chess.c
@@ -2,15 +2,24 @@
 #include "figures.h"
 
 char** flipV(char** y){
-    
+    char *p;
+    p = y;
+    for(int i=0;i<sizeof(y);i++){
+        while(*p != '\0'){
+            y[i] = strev(p);
+            p++;
+        }        
+    }
+    return y;    
 }
 
 char** flipH(char** y){
-    
+    for(int i=sizeof(y); i>=0;i--)
+        return y;
 }
 
 char** rotateL(char** y){
-    
+        
 }
 
 char** rotateR(char** y){
