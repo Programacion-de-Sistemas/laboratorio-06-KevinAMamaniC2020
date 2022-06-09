7fcc2d1cf4801c090119b2b7f5af4526101857f5
KevinAMamaniC2020 <kmamanicondo@unsa.edu.pe>
2022-06-08 20:01:36 -0500

    Se esta llenando la funcion flip H y V

---
+++ chess.c
@@ -0,0 +1,43 @@
+#include "chess.h"
+#include "figures.h"
+
+char** flipV(char** y){
+    
+}
+
+char** flipH(char** y){
+    
+}
+
+char** rotateL(char** y){
+    
+}
+
+char** rotateR(char** y){
+
+}
+
+char** reverse(char** y){
+
+}
+
+char** superImpose(char** y, char** x){
+
+}
+
+char** join(char** y, char** x){
+
+}
+
+char** up(char** y, char** x){
+
+}
+
+char** repeatH(char** y, int x){
+
+}
+
+char** repeatV(char** y, int x){
+
+}
+
